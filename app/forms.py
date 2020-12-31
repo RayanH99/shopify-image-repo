1b19b1b4e870bf683cb8270bf108e0d6f255b12b
Rayan Hussain <hussar9@mcmaster.ca>
2020-12-31 13:57:13 -0500

    added a bunch of stuff, forgot to commit along the way LOL

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileAllowed
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,8 +15,28 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
 
+    def validate_username(self, username):
+        
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username is already in use!')
+
+
+    def validate_email(self, email):
+        
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email is already in use!')
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    image = FileField('Image', validators=[DataRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField('Post')
